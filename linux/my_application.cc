78d37311bb392b28f3d62d523f6d2075a9a1479b
Siavash <kosarisiavash4@gmail.com>
2023-05-19 00:32:26 +0330

    set initial default size for linux version

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -47,8 +47,12 @@ static void my_application_activate(GApplication* application) {
     gtk_window_set_title(window, "sshmagic");
   }
 
-  gtk_window_set_default_size(window, 1280, 720);
+  // I commented two below lines and put my own underneath
+  // gtk_window_set_default_size(window, 1280, 720);
+  // gtk_widget_show(GTK_WIDGET(window));
+  gtk_window_set_default_size(window, 400, 600);
   gtk_widget_show(GTK_WIDGET(window));
+
 
   g_autoptr(FlDartProject) project = fl_dart_project_new();
   fl_dart_project_set_dart_entrypoint_arguments(project, self->dart_entrypoint_arguments);
