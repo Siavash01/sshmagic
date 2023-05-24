a6e4ca3fe721b61d0ae206066eb25a7a177fff49
Siavash <kosarisiavash4@gmail.com>
2023-05-24 04:26:47 +0330

    added drawer

--- lib/main.dart
+++ lib/main.dart
@@ -111,6 +111,44 @@ class _SMagicState extends State<SMagic> {
     );
   }
 
+  Drawer customDrawer(BuildContext context) {
+    return Drawer(
+      // Add a ListView to the drawer. This ensures the user can scroll
+      // through the options in the drawer if there isn't enough vertical
+      // space to fit everything.
+      child: ListView(
+        // Important: Remove any padding from the ListView.
+        padding: EdgeInsets.zero,
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Text('Drawer Header'),
+          ),
+          ListTile(
+            title: const Text('Item 1'),
+            onTap: () {
+              // Update the state of the app
+              // ...
+              // Then close the drawer
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: const Text('Item 2'),
+            onTap: () {
+              // Update the state of the app
+              // ...
+              // Then close the drawer
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -130,7 +168,8 @@ class _SMagicState extends State<SMagic> {
               passwordController: passwordController),
             connectButton(),
           ],
-        )
+        ),
+        drawer: customDrawer(context),
       ),
     );
   }
